93d49336976f16a26431d877c48d361554bdaad3
wedddy0707 <ryoryo.ueda@gmail.com>
2020-11-25 16:25:07 +0900

    distinguish the behavior while training from testing

--- egg/core/reinforce_wrappers_noisy.py
+++ egg/core/reinforce_wrappers_noisy.py
@@ -530,8 +530,11 @@ class SenderReceiverRnnReinforce(nn.Module):
 
     def forward(self, sender_input, labels, receiver_input=None):
         message, log_prob_s, entropy_s = self.sender(sender_input)
-        message_with_noise = self.channel(message)
+
+        message_with_noise = self.channel(message) if self.training else message
+
         message_length = find_lengths(message_with_noise)
+
         receiver_output, log_prob_r, entropy_r = self.receiver(
             message_with_noise, receiver_input, message_length
         )
