8de8be1320c4e2571761ab70502a8efda7f8c20c
wedddy0707 <ryoryo.ueda@gmail.com>
2020-12-10 13:36:11 +0900

    just a small fix

--- egg/core/reinforce_wrappers.py
+++ egg/core/reinforce_wrappers.py
@@ -433,7 +433,7 @@ class SenderReceiverRnnReinforce(nn.Module):
             not_eosed = (i < message_lengths - 1).float()
             effective_entropy_s += entropy_s[:, i] * not_eosed
             effective_log_prob_s += log_prob_s[:, i] * not_eosed
-        effective_entropy_s = effective_entropy_s / torch.clamp(message_lengths, min=1).float()
+        effective_entropy_s = effective_entropy_s / torch.clamp(message_lengths - 1, min=1).float()
 
         weighted_entropy = effective_entropy_s.mean() * self.sender_entropy_coeff + \
                 entropy_r.mean() * self.receiver_entropy_coeff
