eab58a4a20a14544046770f18f977e49a293ce8d
wedddy0707 <ryoryo.ueda@gmail.com>
2020-12-25 21:49:47 +0900

    some modification

--- egg/core/noisy_channel.py
+++ egg/core/noisy_channel.py
@@ -1,15 +1,20 @@
 import torch
 
+
 class Channel():
     def __init__(self, vocab_size, p=0.0):
-        self.p = p
+        if vocab_size < 3:
+            # no replacement will occure
+            self.p = 0.0
+        else:
+            self.p = p * float(vocab_size - 1) / float(vocab_size - 2)
         self.vocab_size = vocab_size
-    
-    def __call__(self, message : torch.Tensor):
+
+    def __call__(self, message: torch.Tensor):
         p = torch.full_like(message, self.p, dtype=torch.double)
 
         repl_choice = torch.bernoulli(p) == 1.0
-        repl_value  = torch.randint_like(message, 1, self.vocab_size)
+        repl_value = torch.randint_like(message, 1, self.vocab_size)
 
         inv_zero_mask = ~(message == 0)
 
